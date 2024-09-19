1015feddb3ff142aac8e4300a1d132a40467645a
RitikAnand02 <ritikanand.02062000@gmail.com>
2024-09-19 20:53:04 +0530

    uuu

---
+++ Java code by hk sir/Test18.java
@@ -0,0 +1,22 @@
+public class Test18 {
+    static void m1() {
+        System.out.println("m1");
+    }
+
+    static int m2() {
+        System.out.println("m2");
+        return 10;
+    }
+
+    public static void main(String[] args) {
+        m1();
+
+        m2();
+
+        int x = m2();
+
+        System.out.println(m2());
+        
+
+    }
+}
